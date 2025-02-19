949b96d465d1abdcfc07ba59472394cbcd9f8a5e
jie-Li-1995 <2893535402@qq.com>
2025-02-19 10:41:18 +0800

    change

--- py/cssToVw.py
+++ py/cssToVw.py
@@ -3,7 +3,6 @@
 import re
 
 # 全局配置
-BASE_WIDTH = 1920  # 基准宽度
 EXCLUDE_PROPERTIES = ['font-size']  # 排除的 CSS 属性
 
 def px_to_vw(css_content, base_width):
@@ -23,7 +22,6 @@ def px_to_vw(css_content, base_width):
             vw_value = (px_value / base_width) * 100
 
             # 格式化为保留两位小数的 vw，且如果是0就不保留小数
-          # 如果是整数，则直接输出整数，若是小数则保留两位小数
             if vw_value == int(vw_value):  # 判断是否是整数
                 return f'{property_name}: {int(vw_value)}vw;'  # 输出整数
             else:
@@ -46,20 +44,48 @@ def process_scss_file(file_path, base_width):
     return px_to_vw(scss_content, base_width)
 
 
-def generate_vw_scss(output_path, main_scss_file, base_width=BASE_WIDTH):
-    """生成转换后的 SCSS 文件"""
-    converted_scss = process_scss_file(main_scss_file, base_width)
+def generate_combined_scss(output_path, scss_files):
+    """将多个 SCSS 文件的转换结果合并为一个 SCSS 文件"""
+    combined_scss = ""
+    mb_scss = ""
 
+    for scss_file in scss_files:
+        mb_scss =""
+        base_width = scss_file['base_width']
+        input_file = scss_file['input']
+        
+        # 获取转换后的 SCSS 内容
+        converted_scss = process_scss_file(input_file, base_width)
+        
+        # 将转换后的内容拼接到最终的 SCSS 字符串
+        combined_scss += f"/* {input_file} 转换结果，基准宽度: {base_width} */\n"
+        if base_width == 750:
+            mb_scss += "@media screen and (max-width: 768px) {\n"
+            mb_scss += converted_scss + "\n\n"
+            mb_scss += "}\n"
+            combined_scss += mb_scss + "\n\n"
+        else:
+             combined_scss += converted_scss + "\n\n"
+            
+
+    # 将合并后的 SCSS 内容写入输出文件
     with open(output_path, 'w', encoding='utf-8') as output_file:
-        output_file.write(converted_scss)
-    print(f"File conversion successful. Output file path: {output_path}")
+        output_file.write(combined_scss)
+    
+    print(f"文件合并并转换成功。输出文件路径：{output_path}")
 
 
 # 示例用法
-main_pc_scss = 'e:/work/jie-Li-1995.github.io/py/pc.scss'  # 主 SCSS 文件路径
-main_mb_scss = 'e:/work/jie-Li-1995.github.io/py/mb.scss'  # 主 SCSS 文件路径
-output_mb_scss = 'e:/work/jie-Li-1995.github.io/py/out/mb_vw.scss'  # 输出的转换后的 SCSS 文件路径
-output_pc_scss = 'e:/work/jie-Li-1995.github.io/py/out/pc_vw.scss'  # 输出的转换后的 SCSS 文件路径
+def process_and_combine_files():
+    """处理多个 SCSS 文件并将它们合并到一个 SCSS 文件"""
+    scss_files = [
+        {'input': 'e:/work/jie-Li-1995.github.io/py/pc.scss', 'base_width': 1920},
+        {'input': 'e:/work/jie-Li-1995.github.io/py/mb.scss', 'base_width': 750}
+    ]
 
-generate_vw_scss(output_pc_scss, main_pc_scss, 1920)
-generate_vw_scss(output_mb_scss, main_mb_scss, 750)
+    output_combined_scss = 'e:/work/jie-Li-1995.github.io/py/out/combined_vw.scss'  # 输出合并后的 SCSS 文件路径
+
+    generate_combined_scss(output_combined_scss, scss_files)
+
+# 运行合并处理函数
+process_and_combine_files()
